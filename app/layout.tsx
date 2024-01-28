14cc60ea3657417e34301cc813726ae4bce2c049
Steve Frost <hello@steveafrost.com>
2024-01-27 18:14:23 -0600

    update root layout, home page, and components

--- app/layout.tsx
+++ app/layout.tsx
@@ -33,7 +33,7 @@ export default function RootLayout({
           fontSans.variable
         )}
       >
-        <main className="flex-1 flex flex-col items-center justify-between px-4 py-8">
+        <main className="flex-1 flex flex-col items-center justify-between px-4 py-8 bg-zinc-50">
           {children}
         </main>
         <nav className="flex flex-row space-x-4 justify-around bg-primary px-4 py-2 border-t-4 border-t-zinc-700/90">
--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,9 @@
 export default function Home() {
   return (
     <div className="flex flex-row space-x-4">
-      <h2 className="text-4xl text-primary font-bold">Tip Track</h2>
+      <h2 className="text-4xl text-transparent font-bold bg-gradient-to-r to-slate-600 from-primary bg-clip-text">
+        Tip Track
+      </h2>
     </div>
   );
 }
