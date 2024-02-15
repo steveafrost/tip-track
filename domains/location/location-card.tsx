c9aa87f26b14db1073c158d90c90d39f2376439a
Steve Frost <hello@steveafrost.com>
2024-02-15 15:28:11 -0600

    update location card to show spelled out tip and emoji

--- domains/location/location-card.tsx
+++ domains/location/location-card.tsx
@@ -4,23 +4,28 @@ import { Card, CardHeader, CardTitle, CardContent } from "@/components/card";
 import { OrdersList } from "../order/order-list";
 import { getOrdersAverageTip } from "../order/order.utils";
 import { useStore } from "@/store";
+import { tipEmoji, tipLabel } from "../tip/tip.constants";
 
 export const LocationCard = () => {
   const { location } = useStore();
 
-  if (Object.keys(location).length === 0)
+  if (Object.keys(location).length === 0) {
     return (
       <p className="font-semibold px-2">
         Search for a location to display all orders and tips associated with
         that address.
       </p>
     );
+  }
+
+  const averageTip = getOrdersAverageTip(location.orders);
 
   return (
     <Card key={location.id}>
       <CardHeader className="pb-4">
-        <CardTitle className="text-lg">
-          Average Tip: {getOrdersAverageTip(location.orders)}
+        <CardTitle className="text-lg flex justify-between">
+          <span>Average: {tipLabel[averageTip.toString()]}</span>
+          <span>{tipEmoji[averageTip.toString()]}</span>
         </CardTitle>
       </CardHeader>
       <CardContent className="px-4">
--- domains/order/order.utils.ts
+++ domains/order/order.utils.ts
@@ -1,5 +1,4 @@
 import { Order } from "@prisma/client";
-import { tipEmoji } from "../tip/tip.constants";
 
 export const getOrdersAverageTip = (orders: Order[]) => {
   if (!orders) return "0";
@@ -7,5 +6,5 @@ export const getOrdersAverageTip = (orders: Order[]) => {
   const totalTips = orders.reduce((acc, order) => acc + (order.tip ?? 0), 0);
   const averageTip = Math.ceil(totalTips / orders.length);
 
-  return tipEmoji[averageTip.toString()];
+  return averageTip.toString();
 };
