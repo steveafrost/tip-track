73a7d526af8997b8be7a33df3f4de6ee8c50928c
Steve Frost <hello@steveafrost.com>
2024-03-08 16:43:37 -0600

    update redirect after signin to new path

--- app/page.tsx
+++ app/page.tsx
@@ -34,8 +34,8 @@ export default function Home() {
             },
           },
         }}
-        afterSignInUrl={"/search"}
-        redirectUrl={"/search"}
+        afterSignInUrl={"/submit"}
+        redirectUrl={"/submit"}
       />
     </div>
   );
